733b53bb569b7e63ae8bca99052a3bd3b8a19cda
Zdenek Janda <zdenek.janda@cloudevelops.com>
2016-01-21 21:51:30 +0100

    increased and nulled db mig

---
+++ db/migrate/20151112145324_create_stack_templates.rb
@@ -0,0 +1,10 @@
+class CreateStackTemplates < ActiveRecord::Migration
+  def change
+#    create_table :stack_templates do |t|
+#      t.string :name
+#      t.text :template
+#
+#      t.timestamps
+#    end
+  end
+end
